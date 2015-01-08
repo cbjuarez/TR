2f6fc57728226ebf43df35d77e00188add0241f3
cbjuarez <cjuarez@unomaha.edu>
2015-01-08 11:56:46 -0600

    Revert "Dropped ui"
    This reverts commit 274d06e1453e492e399e88ca451856438808fbff.

--- ui.r
+++ ui.r
@@ -1,4 +1,4 @@
-fluidPage(
+ui<-fluidPage(
 titlePanel("Taylor-Russell Tables Graphical Demonstration"),
 column(3,hr(),sliderInput('V',"Validity",0,.95,0,.05),
 hr(),sliderInput('BR',"Base Rate",5,95,50,5),
